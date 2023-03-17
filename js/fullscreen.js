0ad86de3621b884a692684f5106121a3ecd7c992
Aqua <115799008+AquaGxmes@users.noreply.github.com>
2023-03-17 06:29:06 -0400

    Update fullscreen.js

--- js/fullscreen.js
+++ js/fullscreen.js
@@ -9,7 +9,15 @@ function openFullscreen() {
   }
 }
 
-localStorage.setItem('gamelink', document.getElementById("aquagame").src);
+var queryString2 = window.location.search;
+var urlParams2 = new URLSearchParams(queryString);
+var gamePage2 = urlParams.get('aqua');
+ jso = fetch('/js/game.json');
+  gms = jso.json();
+  var dat = gms[gamePage]
+function refreshGame() {
+  document.getElementById('aquagame').src = dat.Iframe
+}
 function changeMethod(link) {
   document.getElementById("aquagame").src = link
 }
